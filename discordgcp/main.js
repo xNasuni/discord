8e42b6c2f345070a96a49de739df6dd1817dfd25
xNasuni <62818119+xNasuni@users.noreply.github.com>
2023-06-19 04:34:22 -0400

    fix

--- discordgcp/main.js
+++ discordgcp/main.js
@@ -35,7 +35,7 @@ async function Start() {
             }
         }
 
-        if (Client1GC != null) {
+        if (Client1GC != null && Client2.user != null) {
             var OtherUserFound
 
             Client1GC.recipients.map((UserData, UserID) => {
@@ -48,11 +48,8 @@ async function Start() {
                 console.log(`${Client2.user.username} was not in Group Chat when initializing, they will be added by ${Client1.user.username}.`)
                 try {
                     await Client1GC.addMember(Client2.user)
-                    while (await Client2.channels.resolve(GCID) != null) {
-                        Client2GC = await Client2.channels.resolve(GCID)
-                        console.log(`Set ${Client2.user.username}'s GC to ${Client2GC} from ${Client2.user.username}.`)
-                        break
-                    }
+                    Client2GC = await Client2.channels.resolve(GCID)
+                    console.log(`Set ${Client2.user.username}'s GC to ${Client2GC} from ${Client1.user.username}. ${Client1GC == null ? `(don't worry if it is "null", will fallback)` : ""}`)
                 } catch (err) {
                     console.error(`Groupchat is full (10/10) and ${Client1.user.username} is not able to add ${Client2.user.username} into ${Client1GC}, Stacktrace: \n${err}`)
                     process.exit(1)
@@ -62,7 +59,26 @@ async function Start() {
 
         Client1.on('channelDelete', async (channel) => {
             if (channel.type === DMType && channel.id == GCID) {
-                Client2GC.addMember(Client1.user, true)
+                var FallbackGC;
+                if (Client2GC == null) {
+                    FallbackGC = await ( () => {
+                        return new Promise((Resolve, _) => {
+                            var IntervalID;
+                            IntervalID = setInterval(async () => {
+                                var CheckGC = await Client2.channels.resolve(GCID)
+                                if (CheckGC != null) {
+                                    clearInterval(IntervalID)
+                                    Resolve(CheckGC)
+                                }
+                            }, 100)
+                        })
+                    })()
+                    FallbackGC.addMember(Client1.user)
+                    Client2GC = FallbackGC
+                    console.log(`${Client2.user.username} added back ${Client1.user.username} to ${Client2GC} (fallback set)`)
+                    return;
+                }
+                Client2GC.addMember(Client1.user)
                 console.log(`${Client2.user.username} added back ${Client1.user.username} to ${Client2GC}`)
             }
         })
@@ -80,7 +96,7 @@ async function Start() {
             }
         }
 
-        if (Client2GC != null) {
+        if (Client2GC != null && Client1.user != null) {
             var OtherUserFound
 
             Client2GC.recipients.map((UserData, UserID) => {
@@ -93,11 +109,8 @@ async function Start() {
                 console.log(`${Client1.user.username} was not in Group Chat when initializing, they will be added by ${Client2.user.username}.`)
                 try {
                     await Client2GC.addMember(Client1.user)
-                    while (await Client1.channels.resolve(GCID) != null) {
-                        Client1GC = await Client1.channels.resolve(GCID)
-                        console.log(`Set ${Client1.user.username}'s GC to ${Client1GC} from ${Client1.user.username}.`)
-                        break
-                    }
+                    Client1GC = await Client1.channels.resolve(GCID)
+                    console.log(`Set ${Client1.user.username}'s GC to ${Client1GC} from ${Client2.user.username}. ${Client1GC == null ? `(don't worry if it is "null", will fallback)` : ""}`)
                 } catch (err) {
                     console.error(`Groupchat is full (10/10) and ${Client2.user.username} is not able to add ${Client1.user.username} into ${Client2GC}, Stacktrace: \n${err}`)
                     process.exit(1)
@@ -107,7 +120,26 @@ async function Start() {
 
         Client2.on('channelDelete', async (channel) => {
             if (channel.type === DMType && channel.id == GCID) {
-                Client1GC.addMember(Client2.user, true)
+                if (Client1GC == null) {
+                    var FallbackGC;
+                    FallbackGC = await ( () => {
+                        return new Promise((Resolve, _) => {
+                            var IntervalID;
+                            IntervalID = setInterval(async () => {
+                                var CheckGC = await Client1.channels.resolve(GCID)
+                                if (CheckGC != null) {
+                                    clearInterval(IntervalID)
+                                    Resolve(CheckGC)
+                                }
+                            }, 100)
+                        })
+                    })()
+                    FallbackGC.addMember(Client2.user)
+                    Client1GC = FallbackGC
+                    console.log(`${Client1.user.username} added back ${Client2.user.username} to ${Client1GC} (fallback set)`)
+                    return;
+                }
+                Client1GC.addMember(Client2.user)
                 console.log(`${Client1.user.username} added back ${Client2.user.username} to ${Client1GC}`)
             }
         })
